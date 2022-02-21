68ea7c1f90d0e21bbd8b48b19315f11511fa6875
Galin Manov <thenightarchervclaymore@gmail.com>
2022-02-21 23:23:08 +0200

    mini ORM

--- Spring Data/Custom ORM/src/main/java/orm/DBContext.java
+++ Spring Data/Custom ORM/src/main/java/orm/DBContext.java
@@ -1,13 +1,18 @@
 package orm;
 
+import java.lang.reflect.InvocationTargetException;
+import java.sql.SQLException;
+
 public interface DBContext<E> {
 
-    boolean persist (E entity);
+    boolean persist (E entity) throws IllegalAccessException, SQLException;
 
-    Iterable<E> find (Class<E> table);
-    Iterable<E> find (Class<E> table, String where);
+    Iterable<E> find (Class<E> table) throws SQLException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException;
+    Iterable<E> find (Class<E> table, String where) throws SQLException, InvocationTargetException, InstantiationException, IllegalAccessException, NoSuchMethodException;
 
-    E findFirst(Class<E> table);
-    E findFirst(Class<E> table, String where);
+    E findFirst(Class<E> table) throws SQLException, InvocationTargetException, InstantiationException, NoSuchMethodException, IllegalAccessException;
+    E findFirst(Class<E> table, String where) throws SQLException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException;
+
+    boolean delete(E toDelete) throws IllegalAccessException, SQLException;
 
 }
