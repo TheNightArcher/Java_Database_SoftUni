6c39462d90a6ca02f905c2f16651c67d5a47e423
Galin Manov <thenightarchervclaymore@gmail.com>
2022-02-24 23:09:12 +0200

    First steps into Hibernate

--- Spring Data/Hibernate_JPA/src/main/java/Main.java
+++ Spring Data/Hibernate_JPA/src/main/java/Main.java
@@ -1,6 +1,25 @@
+import entities.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
 
-        
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("school");
+
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Student student = new Student("Galin",21);
+        Student student2 = new Student("Kaloyn",18);
+
+        entityManager.persist(student);
+        entityManager.persist(student2);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
     }
 }
--- Spring Data/Hibernate_JPA/src/main/java/entities/Student.java
+++ Spring Data/Hibernate_JPA/src/main/java/entities/Student.java
@@ -1,4 +1,50 @@
 package entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name", length = 50)
+    private String name;
+
+    @Column(name = "age")
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
