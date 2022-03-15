9bbc710b4fdd2deaeac3b4f7797641f2340904f6
Galin Manov <thenightarchervclaymore@gmail.com>
2022-03-15 22:42:34 +0200

    JSON

---
+++ Spring Data/JSON Processing Exr2/src/test/java/bg/softuni/jsonexr/JsonProcessingExr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.jsonexr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonProcessingExr2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
